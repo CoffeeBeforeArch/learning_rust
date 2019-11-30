789af507452951e3f293856198b0047dadea8728
Nick <CoffeeBeforeArch@gmail.com>
2019-11-30 12:41:18 -0500

    add rust hello world

---
+++ hello_world/main.rs
@@ -0,0 +1,10 @@
+// This program shows off a simple hello world program using Rust!
+// By: Nick from CoffeeBeforeArch
+
+// Similar to C/C++, Rust uses a main function
+fn main() {
+    // "println!" calls a Rust macro
+    // The "!" denotes that this is a macro, and not a function
+    // Rust also uses semicolons to end expressions
+    println!("Hello, world!");
+}
