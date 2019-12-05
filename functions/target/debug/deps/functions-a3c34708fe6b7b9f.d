f6856b1a95ed3d0d47a647cdb439288171c4bc83
Nick <CoffeeBeforeArch@gmail.com>
2019-12-04 20:13:37 -0500

    get rid of a bunch of files that should be ignored

--- functions/target/debug/deps/functions-a3c34708fe6b7b9f.d
+++
@@ -1,5 +0,0 @@
-/home/cba/forked_repos/learning_rust/functions/target/debug/deps/functions-a3c34708fe6b7b9f: src/main.rs
-
-/home/cba/forked_repos/learning_rust/functions/target/debug/deps/functions-a3c34708fe6b7b9f.d: src/main.rs
-
-src/main.rs:
--- functions/target/debug/functions.d
+++
@@ -1,1 +0,0 @@
-/home/cba/forked_repos/learning_rust/functions/target/debug/functions: /home/cba/forked_repos/learning_rust/functions/src/main.rs
